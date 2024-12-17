7c33fda20d5dfbeae5bbd42aeb710cf8514f9792
Jonh2610 <angel.archundia2610@gmail.com>
2024-12-17 11:29:59 -0600

    Update jobscript.sh

--- jobscript.sh
+++ jobscript.sh
@@ -2,9 +2,8 @@
 echo "Ejecutando Despliegue $Ambiente"
 if [ "$agente" = "true" ]
 then
-         for i in 1 2 3 4 5
-         do
-	 echo "El despliegue se realizara en: $i"
+         for i in {1. .5}
+	 echo "El despliegue se realizara en:" $i
          done
 else
 	echo "Disfruta el planeta humano/a $nombre, trata de no morir."
